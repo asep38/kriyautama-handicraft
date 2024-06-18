2140e9808227fab720a6aee21a75d79b3965d987
asep38 <asepzarkasihnoor38@gmail.com>
2024-06-18 15:00:32 +0700

    update: export

--- halaman/barangkeluar/export.php
+++ halaman/barangkeluar/export.php
@@ -1,93 +1,109 @@
-<?php 
+<?php
 require 'functionkeluar.php';
 // require 'cek.php';
 ?>
 <html>
+
 <head>
-  <title>Stock Barang</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <title>Stock Barang</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 </head>
 
 <body>
-<div class="container">
-			<h2>Stock Barang</h2>
-			<h4>(Inventory)</h4>
-				<div class="data-tables datatable-dark">
-					
-                    <table class="table table-bordered" id="mauexport" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Barang</th>
-                                <th>Deskripsi</th>
-                                <th>Stock</th>
-                                
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
+    <div class="container">
+        <h2>Barang Keluar</h2>
+        <h4>(Inventory)</h4>
+        <div class="data-tables datatable-dark">
 
-                        <?php
-                        $ambilsemuadatastock = mysqli_query($conn,"select * from stock");
-                        $i = 1;
-                        while($data=mysqli_fetch_array($ambilsemuadatastock)){
-                            $namabarang = $data['namabarang'];
-                            $deskripsi = $data['deskripsi'];
-                            $stock = $data['stock'];
-                            $idb = $data['idbarang'];
-                            ?>
+            <table class="table table-bordered" id="mauexport" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>tanggal</th>
+                        <th>Nama Barang</th>
+                        <th>Pelanggan</th>
+                        <th>QTY</th>
 
-                            <tr>
-                                <td> <?=$i++;?> </td>
-                                <td> <?php echo $namabarang;?> </td>
-                                <td> <?php echo $deskripsi;?> </td>
-                                <td> <?php echo $stock;?> </td>
-                                
-                            </tr>
+                    </tr>
+                </thead>
 
-                            
+                <tbody>
 
-                        <?php
-                        };
+                    <?php
+                    $ambilsemuadatastock = mysqli_query($conn, "SELECT 
+                    keluar.idkeluar,
+                    keluar.idbarang,
+                    keluar.tanggal,
+                    keluar.idpelanggan,
+                    keluar.qty,
+                    pelanggan.namapelanggan,
+                    stock.namabarang
+                     FROM keluar
+                     JOIN stock ON keluar.idbarang
+                     JOIN pelanggan ON keluar.idpelanggan");
+                    $i = 1;
+                    while ($data = mysqli_fetch_array($ambilsemuadatastock)) {
+                        $idkeluar = $data['idkeluar'];
+                        $idb = $data['idbarang'];
+                        $idpelanggan = $data['idpelanggan'];
+                        $qty = $data['qty'];
+                        $tanggal = $data['tanggal'];
+                        $namabarang = $data['namabarang'];
+                        $namapelanggan = $data['namapelanggan'];
                         ?>
 
-                            
-                        </tbody>
-                    </table>
-					
-	            </div>
-</div>
-	
-<script>
-$(document).ready(function() {
-    $('#mauexport').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'excel', 'pdf', 'print'
-        ]
-    } );
-} );
+                        <tr>
+                            <td> <?= $i++; ?> </td>
+                            <td> <?php echo $tanggal; ?> </td>
+                            <td value="<?php echo $idb; ?> "> <?php echo $namabarang; ?> </td>
+                            <td value="<?php echo $idpelanggan; ?>"> <?php echo $namapelanggan; ?> </td>
+                            <td> <?php echo $qty; ?> </td>
+                        </tr>
 
-</script>
+                        <?php
+                    }
+                    ;
+                    ?>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
 
-	
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            $('#mauexport').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'excel', 'pdf', 'print'
+                ]
+            });
+        });
+
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
+
 
 </body>
 
--- halaman/barangmasuk/export.php
+++ halaman/barangmasuk/export.php
@@ -1,93 +1,112 @@
-<?php 
+<?php
 require 'functionmasuk.php';
 // require 'cek.php';
 ?>
 <html>
+
 <head>
-  <title>Barang Masuk</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
+    <title>Barang Masuk</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 </head>
 
 <body>
-<div class="container">
-			<h2>Barang Masuk</h2>
-			<h4>(Inventory)</h4>
-				<div class="data-tables datatable-dark">
-					
-                    <table class="table table-bordered" id="mauexport" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Nama Barang</th>
-                                <th>Deskripsi</th>
-                                <th>Stock</th>
-                                
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
+    <div class="container">
+        <h2>Barang Masuk</h2>
+        <h4>(Inventory)</h4>
+        <div class="data-tables datatable-dark">
 
-                        <?php
-                        $ambilsemuadatastock = mysqli_query($conn,"select * from stock");
-                        $i = 1;
-                        while($data=mysqli_fetch_array($ambilsemuadatastock)){
-                            $namabarang = $data['namabarang'];
-                            $deskripsi = $data['deskripsi'];
-                            $stock = $data['stock'];
-                            $idb = $data['idbarang'];
-                            ?>
+            <table class="table table-bordered" id="mauexport" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Tanggal</th>
+                        <th>Nama Barang</th>
+                        <th>Pengrajin</th>
+                        <th>Qty</th>
+                    </tr>
+                </thead>
 
-                            <tr>
-                                <td> <?=$i++;?> </td>
-                                <td> <?php echo $namabarang;?> </td>
-                                <td> <?php echo $deskripsi;?> </td>
-                                <td> <?php echo $stock;?> </td>
-                                
-                            </tr>
+                <tbody>
 
-                            
+                    <?php
+                    $ambilsemuadatastock = mysqli_query($conn, "SELECT 
+                    masuk.idbarang,
+                    masuk.idmasuk,
+                    masuk.idpengrajin,
+                    masuk.tanggal, 
+                    masuk.qty, 
+                    stock.namabarang, 
+                    pengrajin.nama 
+                    FROM masuk 
+                    JOIN stock ON masuk.idbarang = stock.idbarang
+                    JOIN pengrajin ON masuk.idpengrajin = pengrajin.idpengrajin");
+                    $i = 1;
+                    while ($data = mysqli_fetch_array($ambilsemuadatastock)) {
+                        $idm = $data['idmasuk'];
+                        $idb = $data['idbarang'];
+                        $idpengrajin = $data['idpengrajin'];
+                        $tanggal = $data['tanggal'];
+                        $qty = $data['qty'];
+                        $namapengrajin = $data['nama'];
+                        $namabarang = $data['namabarang'];
 
-                        <?php
-                        };
                         ?>
 
-                            
-                        </tbody>
-                    </table>
-					
-	            </div>
-</div>
-	
-<script>
-$(document).ready(function() {
-    $('#mauexport').DataTable( {
-        dom: 'Bfrtip',
-        buttons: [
-            'excel', 'pdf', 'print'
-        ]
-    } );
-} );
+                        <tr>
+                            <td> <?= $i++; ?> </td>
+                            <td> <?php echo $tanggal; ?> </td>
+                            <td value="<?php echo $idb; ?>"> <?php echo $namabarang; ?> </td>
+                            <td value="<?php echo $idpengrajin; ?>"> <?php echo $namapengrajin; ?> </td>
+                            <td> <?php echo $qty; ?> </td>
 
-</script>
+                        </tr>
 
-<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
 
-	
+
+                        <?php
+                    }
+                    ;
+                    ?>
+
+
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            $('#mauexport').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'excel', 'pdf', 'print'
+                ]
+            });
+        });
+
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+
+
 
 </body>
 
